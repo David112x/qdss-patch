436bb35aacc92c2b4140d21aa70ca9e1d1636838
David112x <86davidx64@gmail.com>
2023-01-22 12:51:50 -0500

    customize.sh: fix set_permissions

--- customize.sh
+++ customize.sh
@@ -42,6 +42,8 @@ REPLACE="
 
 set_permissions() {
     set_perm_recursive $MODPATH/system/vendor 0 0 0755 0644
+    set_perm_recursive $MODPATH/system/vendor/lib 0 0 0755 0644
+    set_perm_recursive $MODPATH/system/vendor/lib64 0 0 0755 644
     set_perm_recursive $MODPATH/system/product 0 0 0755 0644
     set_perm_recursive $MODPATH/system/system_ext 0 0 0755 0644
     set_perm_recursive $MODPATH/system/system_ext/bin 0 0 0755 0755
